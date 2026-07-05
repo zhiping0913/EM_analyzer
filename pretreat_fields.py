e77f24541d86f7feb47642b80da4939acad273c1
zhiping0913 <zhiping0913@gmail.com>
2026-07-05 19:38:34 -0400

    nearest

--- pretreat_fields.py
+++ pretreat_fields.py
@@ -886,8 +886,8 @@ def _shift_field(field: jnp.ndarray, coordinates: jnp.ndarray) -> jnp.ndarray:
         input=field,
         coordinates=coordinates,
         order=5,
-        mode='constant',
-        cval=0.0,
+        mode='nearest',
+        #cval=0.0,
     )
 
 
